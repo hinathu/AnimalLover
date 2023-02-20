bfc9d4f717b8a64146fbfde60b273f75c152c288
EC2 Default User <ec2-user@ip-172-31-4-196.ap-northeast-1.compute.internal>
2023-02-20 14:42:56 +0000

    管理者用のレイアウト修正

--- app/controllers/admin/customers_controller.rb
+++ app/controllers/admin/customers_controller.rb
@@ -7,6 +7,7 @@ class Admin::CustomersController < ApplicationController
 
   def show
     @customer = Customer.find(params[:id])
+    @items = Item.where(customer_id: @customer.id)
   end
 
   def edit
